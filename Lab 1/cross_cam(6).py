20d6a9254f8bab1bb32c52cbd40efbfef1fa56f5
Asya <cheuzh.asiet@gmail.com>
2025-09-18 22:48:26 +0300

    LAB1: Task 6 complete!

---
+++ Lab 1/cross_cam(6).py
@@ -0,0 +1,26 @@
+import cv2
+
+def drawRedCrossFromCamera():
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        h, w, _ = frame.shape
+        center_x = w // 2
+        center_y = h // 2
+
+        cv2.line(frame, (center_x - 50, center_y), (center_x + 50, center_y), (0, 0, 255), 4)
+        cv2.line(frame, (center_x, center_y - 50), (center_x, center_y + 50), (0, 0, 255), 4)
+
+        cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+drawRedCrossFromCamera()
