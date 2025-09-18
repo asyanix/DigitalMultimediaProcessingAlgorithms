cf109eee1150c8620871a3f6e171e7b89a6c7132
Asya <111695385+asyanix@users.noreply.github.com>
2025-09-18 23:09:50 +0300

    Lab 1: Update task 7

--- Lab 1/video_recorder(7).py
+++ Lab 1/video_recorder(7).py
@@ -6,20 +6,15 @@ def recordVideo():
     # для макоси
     # cap = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION)
 
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    out = cv2.VideoWriter('Lab 1/src/output.avi', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (frame_width, frame_height))
+    out = cv2.VideoWriter('Lab 1/src/output.avi', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (w, h))
 
     while True:
         ret, frame = cap.read()
-        if not ret:
-            print("Ошибка захвата кадра")
-            break
-
         out.write(frame)
         cv2.imshow('frame', frame)
-
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
